c556dba6a63ad8d675d21c901901fc84f3364ea7
Kate Lanina <lnnktrn@gmail.com>
2026-01-10 18:24:12 -0800

    Generated project structure

---
+++ timetravel_java/src/main/java/com/lnnktrn/timetravel_java/TimetravelJavaApplication.java
@@ -0,0 +1,13 @@
+package com.lnnktrn.timetravel_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetravelJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimetravelJavaApplication.class, args);
+    }
+
+}
